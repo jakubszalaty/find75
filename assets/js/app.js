eb57a09cd9f6e839469c258eb125aac1a139d65e
Kuba Szalaty <nnaisur@gmail.com>
2016-12-23 03:10:17 +0100

    First version

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,8 @@
 const app = angular.module( 'App', [
   'ui.router',
   'ngRoute',
-  'ngMaterial'
+  'ngMaterial',
+  'ngMap'
 ])
 
 app.config([
@@ -25,17 +26,128 @@ app.config([
 
 app.controller('IndexController', function ($scope) {})
 
+app.controller('MainController', function ($scope, $mdSidenav, $http, $mdToast, NgMap) {
+  const vm = this
+  vm.title = 'Find 75'
 
-app.controller('MainController', function ($scope, $mdSidenav) {
+  vm.toggleLeft = buildToggler('left')
+  vm.getMatchesStops = getMatchesStops
+  vm.getMatchesBuses = getMatchesBuses
+  vm.setNewCenter = setNewCenter
+  vm.showBus = showBus
+  vm.busStop = null
+  vm.allBuses = false
 
-  $scope.title = 'Hello'
+  getBusStops()
 
-  $scope.toggleLeft = buildToggler('left')
+  getBuses()
+
+
+  function showBus(bus){
+    NgMap.getMap().then((map)=>{
+      map.setCenter({lat: parseFloat(bus.lat), lng: parseFloat(bus.lon)})
+    })
+  }
+
+  function setNewCenter(){
+    document.activeElement.blur()
+    NgMap.getMap().then((map)=>{
+      if(vm.busStop)
+        map.setCenter({lat:vm.busStop.szerokoscgeo, lng: vm.busStop.dlugoscgeo})
+    })
+
+    return
+  }
+
+  function getMatchesBuses(busStop){
+    if(vm.allBuses)
+      return vm.buses
+
+    if(!busStop)
+      return []
+
+    const buses = _.filter(vm.buses, (value)=>{
+      return value.z === busStop.nazwa || value.do === busStop.nazwa
+    })
+    return buses
+  }
+
+  function getMatchesStops(searchText){
+    const search = new RegExp(searchText,'gim')
+    const stops = _.filter(vm.busStops, (value)=>{
+      return value.nazwa.match(search)
+    })
+    return stops
+  }
+
+  function getBusStops(){
+    $http.get('/busStops').then((response)=>{
+      vm.busStops = response.data
+      // showToast('Przystanki pobrane')
+    }).catch((err)=>{
+      showToast('Nie można pobrać przystanków')
+    })
+  }
+
+  function getBuses(){
+    $http.get('/buses').then((response)=>{
+      vm.buses = response.data
+      // showToast('Przystanki pobrane')
+      setTimeout(()=>{
+        getBuses()
+      },5000)
+    }).catch((err)=>{
+      showToast('Aktualizacja pozycji autobusów nie powiodła się')
+    })
+  }
 
   function buildToggler(componentId) {
     return function() {
       $mdSidenav(componentId).toggle()
     }
   }
+
+  function showToast(text){
+    if(!text)
+      return console.error('Value not set')
+    $mdToast.show(
+      $mdToast.simple()
+        .textContent(text)
+        .position('bottom left')
+    )
+  }
+
+  // function getLocation() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(showPosition, showError)
+  //   } else {
+  //     showToast('Geolocation is not supported by this browser.')
+  //   }
+  // }
+
+  // function showPosition(position) {
+  //   // showToast('Latitude: ' + position.coords.latitude + ' Longitude: ' + position.coords.longitude)
+  //   NgMap.getMap().then((map)=>{
+  //     map.setCenter({lat:position.coords.latitude, lng: position.coords.longitude})
+  //   })
+
+  // }
+
+  // function showError(error) {
+  //   switch(error.code) {
+  //   case error.PERMISSION_DENIED:
+  //     showToast('User denied the request for Geolocation.')
+  //     break
+  //   case error.POSITION_UNAVAILABLE:
+  //     showToast('Location information is unavailable.')
+  //     break
+  //   case error.TIMEOUT:
+  //     showToast('The request to get user location timed out.')
+  //     break
+  //   case error.UNKNOWN_ERROR:
+  //     showToast('An unknown error occurred.')
+  //     break
+  //   }
+  // }
 
 })
--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 'use strict'
 
-const PORT = process.env.APP_PORT || 1337
+const PORT = process.env.APP_PORT || 80
 
 const express = require('express')
+const request = require('request-promise')
+
+const _ = require('lodash')
+
 const app = express()
+
+app.use('/', express.static('assets'))
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.use('/', express.static('assets'))
+app.get('/busStops', function (req, res) {
+
+  res.sendFile(__dirname + '/busStops.json')
+  // const obj = {
+  //   uri: 'http://www.zditm.szczecin.pl/json/slupki.inc.php',
+  //   json: true
+  // }
+
+  // request(obj).then(function (response) {
+  //   let data = _.uniqBy(response,'nrzespolu')
+  //   res.json(data)
+  // })
+  // .catch(function (err) {
+  //   res.status(500).json({msg:'Error'})
+  // })
+})
+
+app.get('/buses', function (req, res) {
+
+  // return res.sendFile(__dirname + '/buses.json')
+  const obj = {
+    uri: 'http://www.zditm.szczecin.pl/json/pojazdy.inc.php',
+    json: true
+  }
+
+  request(obj).then(function (response) {
+    res.json(response)
+  })
+  .catch(function (err) {
+    res.status(500).json({msg:'Error'})
+  })
+})
+
 
 app.listen(PORT, function () {
   console.log(`App listening on port ${PORT}!`)
